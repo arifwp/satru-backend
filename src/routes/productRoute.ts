733b562122bae361caf4a64ac03480c2c5d4d001
arifwp <arifwahyu696@gmail.com>
2024-07-09 11:44:33 +0700

    bug fix

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import {
+  createBrand,
   createCategory,
   createProduct,
-  createBrand,
 } from "../controllers/productController";
+import { authMiddlewware } from "../middlewares/authMiddleware";
+import { configUploadProductImages } from "../middlewares/uploadMiddleware";
 
 const productRouter = express.Router();
 
-productRouter.post("/createProduct", createProduct);
-productRouter.post("/createCategory", createCategory);
-productRouter.post("/createBrand", createBrand);
+productRouter.post(
+  "/createProduct",
+  authMiddlewware,
+  configUploadProductImages,
+  createProduct
+);
+productRouter.post("/createCategory", authMiddlewware, createCategory);
+productRouter.post("/createBrand", authMiddlewware, createBrand);
 
 export default productRouter;
