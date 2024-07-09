a66ae0372cd99ebe37a504070f4337a85cb360a6
arifwp <arifwahyu696@gmail.com>
2024-07-09 11:44:17 +0700

    add response helper

---
+++ src/utils/resource.ts
@@ -0,0 +1,25 @@
+import { Response } from "express";
+import fs from "fs";
+import path from "path";
+
+const fsPromises = fs.promises;
+
+export const ResourceDataWithImage = async (
+  status: boolean,
+  errorCode: number,
+  message: string,
+  directory: string,
+  img: any,
+  res: Response
+) => {
+  if (img) {
+    const avatarPath = path.join(__dirname, directory, img);
+    try {
+      await fsPromises.unlink(avatarPath);
+      console.log("unlink succedd");
+    } catch (error: any) {
+      return res.status(500).json({ message: error.message });
+    }
+  }
+  res.status(errorCode).json({ message: message });
+};
