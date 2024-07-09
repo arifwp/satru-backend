e531eb354326bf045a4e49d4491dd9fc0e39b820
arifwp <arifwahyu696@gmail.com>
2024-07-09 16:54:38 +0700

    mengatasi ketololan multer

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -3,19 +3,31 @@ import {
   createBrand,
   createCategory,
   createProduct,
+  updateProduct,
 } from "../controllers/productController";
 import { authMiddlewware } from "../middlewares/authMiddleware";
 import { configUploadProductImages } from "../middlewares/uploadMiddleware";
 
 const productRouter = express.Router();
 
+// PRODUCT
 productRouter.post(
   "/createProduct",
   authMiddlewware,
   configUploadProductImages,
   createProduct
 );
+productRouter.put(
+  "/updateProduct/:productId",
+  authMiddlewware,
+  configUploadProductImages,
+  updateProduct
+);
+
+// CATEGORY
 productRouter.post("/createCategory", authMiddlewware, createCategory);
+
+// BRAND
 productRouter.post("/createBrand", authMiddlewware, createBrand);
 
 export default productRouter;
